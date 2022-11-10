da533623d75a5ea22d405efe874f8b00301bab7c
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-10 17:30:13 -0600

    Added all ExpenseItems to a single container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpensesDisplay from "./components/ExpensesDisplay";
 
 function App() {
   const expenses = [
@@ -25,28 +25,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visiable!</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h1>Let's get started!</h1>
+      <ExpensesDisplay expenses={expenses} />
     </div>
   );
 }
