6f86524770ed426d359e7acef1324de63f92f405
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-15 22:13:05 -0600

    two-way bonding on expenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [date, setDate] = useState('');
@@ -12,26 +11,30 @@ const ExpenseForm = () => {
     setEnteredTitle(e.target.value);
   };
 
-  const amountChangeHandler= (e) => {
+  const amountChangeHandler = (e) => {
     console.log(e.target.value);
     setEnteredAmount(e.target.value);
-  }
+  };
 
   const setDateHandler = (e) => {
     console.log(e);
     setDate(e.target.value);
-  }
+  };
 
-  const  submitHandler = (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
 
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: new Date(date)
-    }
+      date: new Date(date),
+    };
 
     console.log(expenseData);
+
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setDate('');
   };
 
   return (
@@ -39,17 +42,33 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler} min="0.01" step="0.01" />
+          <input
+            type="number"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date"  onChange={setDateHandler} min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            value={date}
+            onChange={setDateHandler}
+            min="2019-01-01"
+            max="2022-12-31"
+          />
         </div>
       </div>
 
