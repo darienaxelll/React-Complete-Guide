b1baf7d7ddc3ac7259791e26c2ef14a67ccb8683
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-28 19:30:23 -0600

    Made the list dynamic

--- src/components/Expenses/ExpensesDisplay.js
+++ src/components/Expenses/ExpensesDisplay.js
@@ -2,39 +2,28 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesDisplay.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const ExpensesDisplay = (props) => {
-  const changeYear = selectedYear => {
-    console.log('In ExpenseDisplay.js');
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const changeYear = (selectedYear) => {
     console.log(selectedYear);
+    setFilteredYear(selectedYear);
   };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter onChangeYear={changeYear} />
+        <ExpensesFilter selected={filteredYear} onChangeYear={changeYear} />
 
-        <ExpenseItem
-          expenses={props.expenses}
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
