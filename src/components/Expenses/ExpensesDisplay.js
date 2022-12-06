c536f821c48d81f88209828b59aadf620f80d1a3
Darien Salazar <darien.axell.salazar@gmail.com>
2022-12-05 22:40:03 -0600

    Added conditional statement to filter years with no items.

--- src/components/Expenses/ExpensesDisplay.js
+++ src/components/Expenses/ExpensesDisplay.js
@@ -8,27 +8,32 @@ const ExpensesDisplay = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const changeYear = (selectedYear) => {
-    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.expenses.filter(expense => {
+  const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeYear={changeYear} />
-
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesFilter 
+          selected={filteredYear} 
+          onChangeYear={changeYear} 
+        />
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
